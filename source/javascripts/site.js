a94f294cb1be80543cd3cf1ee2b622627c61b68c
matsuhisa <matsuhisa+github@gmail.com>
2019-07-14 22:31:53 +0900

    Use stimulus.js

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,7 @@
-// This is where it all goes :)
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("./controllers/answers", true, /\.js$/)
+// const context = require.context('./controllers', true, /^\.\/.*\.js$/)
+application.load(definitionsFromContext(context))
