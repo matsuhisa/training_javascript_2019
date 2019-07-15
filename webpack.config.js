03642f4ca556fca62c03f37d1c20af7e350fb441
matsuhisa <matsuhisa+github@gmail.com>
2019-07-15 23:23:29 +0900

    Use babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,35 @@
 const webpack = require('webpack');
 
 module.exports = {
-  mode: 'development',
-  entry: [
-    './source/javascripts/site.js'
-  ],
+  entry: {
+    bundle: ['./source/javascripts/site.js']
+  },
+
   output: {
     filename: 'bundle.js',
     path: __dirname + '/build/javascripts'
+  },
+
+  mode: "development",
+  // mode: "production",
+  // devtool: "source-map",
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                // プリセットを指定することで、ES2019 を ES5 に変換
+                "@babel/preset-env"
+              ]
+            }
+          }
+        ]
+      }
+    ]
   }
 };
