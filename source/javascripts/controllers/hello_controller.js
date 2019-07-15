8f9277beb685517daedd0b52d824e958aa173ad6
matsuhisa <matsuhisa+github@gmail.com>
2019-07-16 00:05:10 +0900

    modifier hello_controller.js

--- source/javascripts/controllers/hello_controller.js
+++ source/javascripts/controllers/hello_controller.js
@@ -1,7 +1,14 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
+  static targets = [ "name", "output" ]
+
   connect() {
     console.log("Hello, Stimulus!", this.element)
+  }
+
+  greet() {
+    this.outputTarget.textContent =
+      `Hello, ${this.nameTarget.value}!`
   }
 }
