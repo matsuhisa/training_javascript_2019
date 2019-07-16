97ebb60aa4421b0f22feea9ece1cbd588dc3d5ab
matsuhisa <matsuhisa+github@gmail.com>
2019-07-16 20:47:22 +0900

    Add dialog_controller.js

---
+++ source/javascripts/controllers/dialog_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = [ "background", "body" ]
+
+  toggle() {
+    const background = this.backgroundTarget
+    const body = this.bodyTarget
+
+    if( body.getAttribute('aria-disabled') === 'true' ) {
+      body.setAttribute('aria-disabled', false)
+      background.setAttribute('aria-disabled', false)
+    }else {
+      body.setAttribute('aria-disabled', true)
+      background.setAttribute('aria-disabled', true)
+    }
+  }
+}
