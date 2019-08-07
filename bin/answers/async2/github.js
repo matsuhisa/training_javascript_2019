5f0a164220889c0dd8158e4ccecfce774d106e1c
matsuhisa <matsuhisa+github@gmail.com>
2019-08-07 23:41:23 +0900

    Tweak github.js

--- bin/answers/async2/github.js
+++ bin/answers/async2/github.js
@@ -1,10 +1,9 @@
 const fetch = require('node-fetch')
 
 fetch(`https://api.github.com/users/matsuhisa`)
-    .then(response => {
-        console.log(response.status); // => 200
-        response.json().then(userInfo => {
-            // JSONパースされたオブジェクトが渡される
-            console.log(userInfo); // => {...}
-        });
+  .then(response => {
+    console.log(response.status)
+    response.json().then(userInfo => {
+      console.log(userInfo)
     });
+  });
