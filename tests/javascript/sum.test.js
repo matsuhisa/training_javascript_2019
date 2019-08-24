6535f65e8ea858a1e5ad2cf12ae42ae70a31a253
matsuhisa <matsuhisa+github@gmail.com>
2019-08-24 10:16:16 +0900

    Move test files

--- tests/javascript/sum.test.js
+++ tests/javascript/sum.test.js
@@ -1,4 +1,4 @@
-const sum = require('../../source/javascripts/sum');
+const sum = require('../../source/javascripts/for_test_sample/sum');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
